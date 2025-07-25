802e7b5ea2e0f4bca9dc0c95c88ba5ca77023aa4
GuLu <xiulinghuang136@gmail.com>
2025-07-24 21:12:35 -0700

    update function_lib

--- System/function_lib/dynamic_lib/hello.c
+++ System/function_lib/dynamic_lib/hello.c
@@ -7,8 +7,8 @@ int mul(int a, int b);
 int main(int argc, char *argv[]) {
 
     printf("%d + %d = %d\n", 1, 2, add(1, 2));
-    printf("%d - %d = %d\n", 10, 2, add(10, 2));
-    printf("%d * %d = %d\n", 10, 2, add(10, 2));
+    printf("%d - %d = %d\n", 10, 2, sub(10, 2));
+    printf("%d * %d = %d\n", 10, 2, mul(10, 2));
 
     return 0;
 }
