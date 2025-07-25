586ff1e65f5b531d3d9833350280d2f707847d1c
GuLu <xiulinghuang136@gmail.com>
2025-07-24 21:17:21 -0700

    static_lib

---
+++ System/function_lib/static_lib/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+
+int main(int argc, char *argv[]) {
+
+    printf("%d + %d = %d\n", 1, 2, add(1, 2));
+    printf("%d - %d = %d\n", 10, 2, sub(10, 2));
+    printf("%d * %d = %d\n", 10, 2, mul(10, 2));
+
+    return 0;
+}
