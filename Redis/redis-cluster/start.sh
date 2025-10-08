c4e5f0cef36146f19e615bdbb6828926672783cc
GuLu <xiulinghuang136@gmail.com>
2025-10-08 06:49:46 -0700

    redis-cluster

---
+++ Redis/redis-cluster/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+redis-server 7001/7001.conf
+redis-server 7002/7002.conf
+redis-server 7003/7003.conf
+redis-server 7004/7004.conf
+redis-server 7005/7005.conf
+redis-server 7006/7006.conf
